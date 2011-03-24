d85c3538e54f739e718c2d645b280cc60aa422e1
Dave Filion <drf259@nyu.edu>
2011-03-23 20:50:50 -0400

    commit

--- phpmyadmin/config/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$i=0;
-$i++;
-$cfg['Servers'][$i]['user']          = 'root';
-$cfg['Servers'][$i]['password']      = 'root'; // use here your password
-$cfg['Servers'][$i]['auth_type']     = 'config';
-?>
